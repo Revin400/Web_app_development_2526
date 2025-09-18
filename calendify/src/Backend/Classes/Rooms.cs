7b534689f6a5020fd3f26f829b03ed4edca7c215
JaySurj <2451shivvaran@gmail.com>
2025-09-18 12:40:09 +0200

    Startup
    `React startup

---
+++ calendify/src/Backend/Classes/Rooms.cs
@@ -0,0 +1,9 @@
+public class Room
+{
+    public int RoomId { get; set; }
+
+    public string Name { get; set; }
+    public int Capacity { get; set; }
+    
+    public string Location { get; set; }
+}
