114138f5858cf1456783e66f9ead9d9257501537
Younes Ikajouen <1078202@hr.nl>
2025-10-02 11:04:29 +0200

    commit whole login page

--- calendify/src/App.js
+++ calendify/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import LoginPage from './Login';
 function App() {
--- calendify/src/Login.jsx
+++ calendify/src/Login.jsx
@@ -1,33 +1,14 @@
-import { useEffect, useState } from "react";
 import "./login.css";
 import calendifylogo from "./calendifylogo.png";
 export default function LoginPage() {
-  const [timestamp, setTimestamp] = useState("");
 
-  useEffect(() => {
-    function updateTimestamp() {
-      const now = new Date();
-      const day = String(now.getDate()).padStart(2, "0");
-      const month = String(now.getMonth() + 1).padStart(2, "0");
-      const year = now.getFullYear();
-      const hours = String(now.getHours()).padStart(2, "0");
-      const minutes = String(now.getMinutes()).padStart(2, "0");
-      setTimestamp(`${day}-${month}-${year} ${hours}:${minutes}`);
-    }
-
-    updateTimestamp();
-    const interval = setInterval(updateTimestamp, 10000);
-    return () => clearInterval(interval);
-  }, []);
 
   return (
     <div className="body">
-      <div className="header-container">
-        <div>{timestamp}</div>
-      </div>
+
 
       <div className="logo-container">
-<img src={calendifylogo} alt="Calendify Logo" className="logo-image" />
+      <img src={calendifylogo} alt="Calendify Logo" className="logo-image" />
         <span className="logo-text">Calendify</span>
       </div>
 
