a4e56914b2ebde22f164db6d02667b1f3866ecff
Younes Ikajouen <1078202@hr.nl>
2025-10-28 15:37:20 +0100

    roompage
    izan

---
+++ calendify/src/RoomPage.jsx
@@ -0,0 +1,45 @@
+import "./RoomPage.css";
+
+export default function AvailableRooms() {
+  return (
+    <div className="modal">
+      <div className="modal-header">
+        <button className="close-btn">X</button>
+        <h5 className="modal-title">Available rooms</h5>
+      </div>
+
+      <div className="table-header">
+        <span>Rooms</span>
+        <div className="datetime">
+          <input type="date" defaultValue="Date" />
+          <input type="time" defaultValue="Time" />
+        </div>
+        <span>Availability</span>
+      </div>
+
+      <div className="table-body">
+        <div className="row">
+          <div className="cell">Room 1</div>
+          <div className="cell available">Available</div>
+        </div>
+
+        <div className="row">
+          <div className="cell">Room 2</div>
+          <div className="cell not-available">Not available</div>
+        </div>
+
+        <div className="row">
+          <div className="cell">Room 3</div>
+          <div className="cell available">Available</div>
+        </div>
+
+        <div className="row">
+          <div className="cell">Room 4</div>
+          <div className="cell not-available">Not available</div>
+        </div>
+      </div>
+
+      <button className="confirm-btn">confirm</button>
+    </div>
+  );
+}
